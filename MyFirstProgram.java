e9cd03d8817c34e7aa7ed62e448feca2900aaa46
omimaadlan <99827713+omimaadlan@users.noreply.github.com>
2022-02-16 16:18:50 -0500

    Add files via upload

---
+++ MyFirstProgram.java
@@ -0,0 +1,11 @@
+public class MyFirstProgram {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        MyFirstProgram myFirstProgram = new MyFirstProgram();
+                myFirstProgram.printHello();
+
+    }
+public void printHello(){
+        System.out.println("Hello World!");
+}
+}
